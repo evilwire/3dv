4a2fdf7590db9a7bc65b3d0ce88a50cf0bebf14d
Knight Fu <knight.w.fu@gmail.com>
2014-06-03 11:00:36 -0400

    added Scale class

--- js/ss-plot/model/range.js
+++ js/ss-plot/model/range.js
@@ -6,8 +6,36 @@
       max : 40
     },
 
+    validate : function()
+    {
+      if( max <= min )
+        return "Range error: Max is not greater than min";
+    },
+
     initialize : function(){
 
+    }
+  });
+
+  var ScaleInitMethods = {
+    'fixed' : function( params )
+    {
+      return {
+        ticSize : params.ticSize,
+      }
+    },
+  };
+
+  SSPlot.Scale = Backbone.Model.extend({
+    defaults : {
+      type : 'fixed',
+      ticSize : 20,
+    },
+
+    initilize : function(){
+      var initMethod = ScaleInitMethods[this.get('type')],
+      scaleAttr = initMethods.call( this, this.attributes )
+      this.set(scaleAttr, { silent : true });
     }
   });
 })(Backbone);
