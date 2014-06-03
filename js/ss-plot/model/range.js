346b8153efa98146159d6ac04a36939c6f9479e5
Knight Fu <knight.w.fu@gmail.com>
2014-06-03 09:16:44 -0400

    added the range model

--- js/ss-plot/model/range.js
+++ js/ss-plot/model/range.js
@@ -1,4 +1,13 @@
 (function(Backbone)
 {
-  console.log( Backbone );
+  SSPlot.Range = Backbone.Model.extend({
+    defaults : {
+      min : 0,
+      max : 40
+    },
+
+    initialize : function(){
+
+    }
+  });
 })(Backbone);
