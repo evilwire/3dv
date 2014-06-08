f373e4a96741fbc1f912138d91ac847109175a21
Knight Fu <knight.w.fu@gmail.com>
2014-06-07 20:49:01 -0400

    added the XYData collection class

--- js/ss-plot/model/2ddata.js
+++ js/ss-plot/model/2ddata.js
@@ -1,6 +1,12 @@
 (function( Backbone )
 {
+  var XYData =
   SSPlot.Model.XYData = Backbone.Model.extend({
 
   });
+
+  SSPlot.Collection.XYDataCollection = 
+    Backbone.Collection.extend({
+      model : XYData
+    });
 })( Backbone );
