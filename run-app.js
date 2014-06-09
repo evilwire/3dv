3b0823e3361892a6a6b1831f31abf4cfa1e8a0e0
Knight Fu <knight.w.fu@gmail.com>
2014-06-08 21:18:17 -0400

    added mouse event control

--- run-app.js
+++ run-app.js
@@ -3,11 +3,8 @@
 
 $(document).ready( function(){
   var paper = Raphael($('.canvas')[0]),
-
   xRange = new SSPlot.Model.Range({ }), 
-
   xScale = new SSPlot.Model.Scale({ }),
-
   xAxis = new SSPlot.Model.Axis({
     scale : xScale,
     range : xRange,
@@ -15,9 +12,7 @@ $(document).ready( function(){
   }),
 
   yRange = new SSPlot.Model.Range({ }), 
-
   yScale = new SSPlot.Model.Scale({ }),
-
   yAxis = new SSPlot.Model.Axis({
     scale : yScale,
     range : yRange,
@@ -30,23 +25,29 @@ $(document).ready( function(){
     height: 540,
     width: 800
   }),
-
   data = new SSPlot.Collection.XYDataCollection(),
-
   xyPlot = new SSPlot.Model.XYPlot({
     haxis : xAxis,
     vaxis : yAxis,
     bbox : bBox,
     data : data,
   }),
-
   xyPlotView = new SSPlot.View.XYPlot({
     paper : paper,
     model : xyPlot
   });
-
   xyPlot.getInitialValues();
   xyPlotView.render();
+
+  $( document ).on('select', function( event )
+  {
+    event.preventDefault();
+  } );
+
+  $( document ).on('mousedown', function( event )
+  {
+    event.preventDefault();
+  } );
 } );
 
 })();
