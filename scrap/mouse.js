d8e9c657dd1ce87ade9eba87efa44dcb78958d0c
Knight Fu <knight.w.fu@gmail.com>
2014-06-21 22:18:28 -0400

    added some scrap to play with some ideas

---
+++ scrap/mouse.js
@@ -0,0 +1,62 @@
+$( document ).ready( function( event )
+{
+  // do something about
+  var canvasDIV = document.getElementById('canvas');
+  paper = Raphael(canvasDIV, '100%', '100%');
+  
+  $( canvasDIV ).css({
+    'height' : '800px',
+    'background-color' : '#eee'
+  });
+
+  // create the dots
+  for( var i = 0; i < 56; i++ )
+    for( var j = 0; j < 40; j++ )
+    {
+      var circle = paper.circle(
+        i * 20,
+        j * 20,
+        4
+      ).attr({fill: '#000'});
+    }
+  
+  var left = 300;
+  var top = 300;
+  paper.setViewBox( left, top, 150, 150, false );
+  
+  var updateViewBox = function( event )
+  {
+    // update the view box
+    var deltaX = event.pageX - initX,
+        deltaY = event.pageY - initY; 
+    
+    paper.setViewBox( left - deltaX, 
+      top - deltaY, 150, 150, false);
+  },
+
+  documentMouseUp = function( event )
+  {
+    $( document ).off('mousemove',
+      updateViewBox );
+    
+    $( document ).off('mouseup',
+      documentMouseUp );
+  },
+
+  initX,
+
+  initY;
+
+  $( document ).on( 'mousedown',
+  function( event )
+  {
+    initX = event.pageX;
+    initY = event.pageY;
+
+    $( document ).on( 'mousemove',
+      updateViewBox);
+
+    $( document ).on( 'mouseup',
+      documentMouseUp );
+  });
+} );
